4c3b1d335586a181ad6e72c87f0c9595b98b483b
Dmitriy <stdimensiy@yandex.ru>
2021-07-20 00:28:28 +0300

    Изм 5. - наконецто разобрался с bottomappbar (был конфликт), в итоге теперь навигация работает корректно. Сохранена и активно работет боковая шторка менбшки и bottomappbar как на уроке. - удалил более не нужный файл разметки app_bar_main.xml - внес необходимые изменения в файл разметки activity_main.xml - удалено лишнее в манифесте - почищен MainActivity.kt - добавлены методы работы с bottomappbar во фрагменте. (базовая идея в том, что каждый фрагмент будет иметь свой bottomappbar и общую боковую шторку меню) - работают стили (взяты из примера изменения минимальные) - убраны лишние трансформации картинки picasso, теперь картинка не деформируется. - в MainActivity.kt переопределен метод onSupportNavigateUp теперь гамбургер во фрагменте вызывает открывание шторки, и навконтроллер вытащил в поле класса (lattinnit) с инициализацией (отложенной) в onCreate

--- app/src/main/java/ru/vdv/myapp/beautifulpictureseveryday/MainActivity.kt
+++ app/src/main/java/ru/vdv/myapp/beautifulpictureseveryday/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,30 +27,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
-
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+       navView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/ru/vdv/myapp/beautifulpictureseveryday/ui/home/HomeFragment.kt
+++ app/src/main/java/ru/vdv/myapp/beautifulpictureseveryday/ui/home/HomeFragment.kt
@@ -2,14 +2,14 @@ package ru.vdv.myapp.beautifulpictureseveryday.ui.home
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.squareup.picasso.Picasso
+import ru.vdv.myapp.beautifulpictureseveryday.MainActivity
 import ru.vdv.myapp.beautifulpictureseveryday.R
 import ru.vdv.myapp.beautifulpictureseveryday.databinding.FragmentHomeBinding
 
@@ -34,13 +34,11 @@ class HomeFragment : Fragment() {
         val imageView: ImageView = binding.imageViewApod
         homeViewModel.apodLiveData.observe(viewLifecycleOwner, {
             Log.d("Моя проверка", " пытаюсь вывести результат")
-            //textView.text = it.explanation
+            textView.text = it.title
             Picasso.get()
                 .load(it.url)
                 .placeholder(R.drawable.apod_pholder)
                 .error(R.drawable.err404)
-                .resize(1080, 2340)
-                .centerCrop()
                 .into(imageView)
         })
         return root
@@ -50,10 +48,31 @@ class HomeFragment : Fragment() {
         Log.d("Моя проверка", " сработал onViewCreated")
         super.onViewCreated(view, savedInstanceState)
         homeViewModel.fetchData()
+        setBottomAppBar(view)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_favorite_flag -> Toast.makeText(context, "Нажата иконка \"В избранное\"", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_api -> Toast.makeText(context, "Нажата иконка \"Телескоп\"", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_settings -> Toast.makeText(context, "Нажата иконка \"Настройки\"", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
     }
 }
